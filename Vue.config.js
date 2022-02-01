e0bc65ca1241a5fbb9abdfcd52ac8ff390f3f137
Nobyri <bonbikbik@gmail.com>
2022-02-01 18:55:19 +0900

    docsフォルダ追加（Gitpages用）

--- Vue.config.js
+++ Vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/weather-app/'   //任意
-        : '/'
+        ? '/'   //任意
+        : '/',
+    outputDir: 'docs/', //これ
 }
