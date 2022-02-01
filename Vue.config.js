01193644e8b1d1849129d083f54e3dae1ec87ad9
Nobyri <bonbikbik@gmail.com>
2022-02-01 20:18:52 +0900

    再ビルド

--- Vue.config.js
+++ Vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/'   //任意
-        : '/',
-    outputDir: 'docs/', //これ
+    publicPath: '/web_roulette',
+    outputDir: 'docs', //これ
 }
